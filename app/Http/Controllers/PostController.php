1ae57b87bff72653b133829e01b53d6c407e927b
Haruto9981 <aketaharuto46@icloud.com>
2023-07-29 17:42:56 +0900

    ninth commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Http\Requests\PostRequest; 
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -17,11 +18,7 @@ class PostController extends Controller
         return view('posts.show')->with(['post' => $post]);
     }
 
-    public function create()
-    {
-        return view('posts.create');
-    }
-
+  
     public function store(Post $post, PostRequest $request) 
     {
         $input = $request['post'];
@@ -45,6 +42,11 @@ class PostController extends Controller
     {
         $post->delete();
         return redirect('/');
+    }
+    
+    public function create(Category $category)
+    {
+        return view('posts/create')->with(['categories' => $category->get()]);
     }
 
 }
